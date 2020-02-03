2884f6459c0a37f6eb05fb987250d076fc3260c2
Kathleen Wang <kat@Kathleens-MacBook-Air.local>
2020-02-02 20:31:38 -0500

    filter

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const url = 'https://data.cityofnewyork.us/resource/f9bf-2cp4.json'
 let test = async function () {
   await axios.get(url).then(response => {
-    console.log(response)
+    let filtered = response.data.filter((x) => {
+      return parseInt(x.num_of_sat_test_takers) > 300
+
+    })
+    console.log(filtered)
   }).catch(e => {
     console.log(e)
   })
